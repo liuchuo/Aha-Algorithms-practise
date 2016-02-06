52e1cf9e04e2c1a8df6006676485f20e2e7ca82c
陈欣 <95323362@qq.com>
2016-02-06 16:14:19 +0800

    用结构体存储名字和分数两个量

--- 1.2冒泡排序.cpp
+++ 1.2冒泡排序.cpp
@@ -25,3 +25,32 @@ int main() {
     }
     return 0;
 }
+
+//如果要排序的是对应分数的人的名字的话，就用结构体存储名字和分数两个量
+#include <iostream>
+using namespace  std;
+struct student {
+    char name[21];
+    int score;
+};
+int main() {
+    student a[100], temp;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i].name >> a[i].score;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            if (a[j].score > a[j + 1].score) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << a[i].name << " " << a[i].score << endl;
+    }
+    return 0;
+}
