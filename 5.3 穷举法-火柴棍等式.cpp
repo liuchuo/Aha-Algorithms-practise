3d436f742a222ceae9df97759d5af5960e0e5e9a
陈欣 <95323362@qq.com>
2016-02-12 22:47:29 +0800

    2

--- 5.3 穷举法-火柴棍等式.cpp
+++ 5.3 穷举法-火柴棍等式.cpp
@@ -1,1 +1,21 @@
 5.3 穷举法-火柴棍等式
+m根火柴棍可以拼出多少种A+B=C的等式？
+下面是各个数字或者符号需要的火柴数
+火柴必须全部用掉  m <= 24
+0 -> 6  1 -> 2  2 -> 5
+3 -> 5  4 -> 4  5 -> 5
+6 -> 6  7 -> 3  8 -> 7
+9 -> 6  + -> 2  = -> 2
+
+#include <iostream>
+using namespace std;
+int func(int x) {
+	
+}
+int main() {
+	int m;
+	cin >> m;
+	
+
+	return 0;
+}
