12a5a5f8702c7ef63836e41a13f5c465f9a6b3bb
陈欣 <95323362@qq.com>
2016-02-12 22:18:07 +0800

    3.1 穷举法

--- 3.1穷举法.cpp
+++ 3.1穷举法.cpp
@@ -1,6 +1,10 @@
 3.1 穷举法
 xxx + xxx = xxx
 x填入1~9数字，使得等式成立，数字不得重复
+直接判断abcdefghi互相不相等的方法太繁琐，可以用book数组标记
+如果这个数字出现过了，那么就把这个数字对应的book值标记为1
+那么根据1~9下标的数组是否都标记为1(sum的值是否等于9)来判断abcdefghi是否两两不相等
+用数组a[i]表示1~9会更简便易读。
 #include <iostream>
 using namespace std;
 int main() {
