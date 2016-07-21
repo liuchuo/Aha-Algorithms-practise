e628c156cb1e98663a8728d3ca51079658bfdb4b
陈欣 <95323362@qq.com>
2016-07-21 22:29:39 +0800

    -

--- 6.4 Bellman-Ford——解决负权边.cpp
+++ 6.4 Bellman-Ford——解决负权边.cpp
@@ -22,41 +22,42 @@ if (flag == 1)
 //n个结点，m条边
 using namespace std;
 int main() {
-	int dis[10], bak[10], n, m, u[10], v[10], w[10], check, flag;
-	int inf = 99999999;
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= m; i++)
-		scanf("%d %d %d", &u[i], &v[i], &w[i]);
-
-	for (int i = 1; i <= n; i++)
-		dis[i] = inf;
-	dis[1] = 0;
-
-	for (int k = 1; k <= n - 1; k++) {
-		for (int i = 1; i <= n; i++)
-			bak[i] = dis[i];
-		for (int i = 1; i <= m; i++)
-			if (dis[v[i]] > dis[u[i]] + w[i])
-				dis[v[i]] = dis[u[i]] + w[i];
-		check = 0;
-		for (int i = 1; i < n; i++)
-			if (bak[i] != dis[i]) {
-				check = 1;
-				break;
-			}
-		if (cheak == 0)
-			break;
-	}
-
-	flag = 0;
-	for (int i = 1; i <= m; i++)
-		if (dis[v[i]] > dis[u[i]] + w[i])
-			flag = 1;
-		if (flag == 1)
-			printf("此图含有负权回路");
-		else {
-			for (int i = 1; i <= n; i++) 
-				printf("%d ", dis[i]);
-		}
-		return 0;
+    int dis[10], bak[10], n, m, u[10], v[10], w[10], check, flag;
+    int inf = 99999999;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; i++)
+        scanf("%d %d %d", &u[i], &v[i], &w[i]);
+    
+    for (int i = 1; i <= n; i++)
+        dis[i] = inf;
+    dis[1] = 0;
+    
+    for (int k = 1; k <= n - 1; k++) {
+        for (int i = 1; i <= n; i++)
+            bak[i] = dis[i];//将dis数组备份入bak数组中，为了下面的检查是否更新而进行剪枝
+        for (int i = 1; i <= m; i++)
+            if (dis[v[i]] > dis[u[i]] + w[i])
+                dis[v[i]] = dis[u[i]] + w[i];
+        check = 0;
+        for (int i = 1; i < n; i++)
+            if (bak[i] != dis[i]) {
+                check = 1;
+                break;
+            }
+        // 如果对于所有的结点，dis的值都没有被更新，那就提前跳出循环，说明不需要进行n-1次了
+        if (check == 0)
+            break;
+    }
+    //检查是否含有负权回路（可省略）
+    flag = 0;
+    for (int i = 1; i <= m; i++)
+        if (dis[v[i]] > dis[u[i]] + w[i])
+            flag = 1;
+    if (flag == 1)
+        printf("此图含有负权回路");
+    else {
+        for (int i = 1; i <= n; i++) 
+            printf("%d ", dis[i]);
+    }
+    return 0;
 }
